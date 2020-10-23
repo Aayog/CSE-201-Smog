8a64822fef39e49c2e60b7d5dc223649b7fb62a4
Aayog <1aayogkoirala@gmail.com>
2020-10-23 15:06:28 -0400

    Php working with database

--- php/config.php
+++ php/config.php
@@ -17,7 +17,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
+
 // $sql = "SELECT username, password FROM User";
 // $result = $conn->query($sql);
 
