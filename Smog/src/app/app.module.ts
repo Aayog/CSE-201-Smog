3939f92ce01df63c3dacc9a3d75f9c04a3ed2106
Aayog <1aayogkoirala@gmail.com>
2020-10-23 00:56:49 -0400

    Updated login component ---still working on it

--- Smog/src/app/app.module.ts
+++ Smog/src/app/app.module.ts
@@ -40,6 +40,7 @@ import { AppRoutingModule } from './app.routing';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ChartsModule } from 'ng2-charts';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -54,7 +55,8 @@ import { ChartsModule } from 'ng2-charts';
     PerfectScrollbarModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
-    ChartsModule
+    ChartsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
--- Smog/src/app/views/login/login.component.ts
+++ Smog/src/app/views/login/login.component.ts
@@ -1,7 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+// import ( first ) from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
+//ref: https://www.mtutorial.com/angular-login-logout-registration-example-php-api
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit{
+  angForm: FormGroup;
+  // , private dataService: DataserviceService,
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.angForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.pattern("[A-Za-z0-9!@#$^&*]*")]]
+    });
+  }
+
+  ngOnInit(){}
+
+  postdata(angForm1: NgForm){
+    if (angForm1.value.username === "admin" && angForm1.value.password == "admin"){
+      this.router.navigate(["/dashboard"]);
+    }
+  }
+  // {
+  //   this.dataService.userlogin(angForm1.value.username, angForm1.value.password)
+  //     .pipe(first())
+  //     .subscribe(
+  //         data => {
+  //           const redirect = this.dataService.redirectUrl ? this.dataService.redirectUrl : '/dashboard';
+  //           this.router.navigate([redirect]);
+  //         },
+  //         error => {
+  //           alert("Username or password incorrect")
+  //         }
+  //     );
+  // // }
+  get username() { return this.angForm.get('username');}
+  get password() { return this.angForm.get('password');}
+ }
