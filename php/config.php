d13e7247b723e1294a3a72d65c96333331401e05
Connor Redwine <redwineconnor@gmail.com>
2020-11-16 15:42:00 -0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,9 +1,13 @@
 <?php
+//Configuration
+//No input
+//It attempts to link up with the server, and creates a connection.
+//No output
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 //ip address for servername
-//if that doesn’t work 
+//if that doesn’t work
 //phrasal-concord-292413:us-central1:mustain-99
 $servername = "104.154.66.238";
 $username = "root";
