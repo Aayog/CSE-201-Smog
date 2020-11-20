4e1e4a513936102ebc9dc45aa652d72c3aeeb1cb
Aayog <1aayogkoirala@gmail.com>
2020-11-20 00:56:38 -0500

    Added review request

---
+++ php/getrequests.php
@@ -0,0 +1,26 @@
+<?php
+# https://phpenthusiast.com/blog/develop-angular-php-app-getting-the-list-of-items
+
+include_once("config.php");
+
+$sql = "SELECT * FROM Requests";
+$requests = [];
+
+if($result = mysqli_query($conn,$sql))
+{
+  $cr = 0;
+  while($row = mysqli_fetch_assoc($result))
+  {
+    $requests[$cr]['Title'] = $row['gTitle'];
+    $requests[$cr]['Descript'] = $row['gDescript'];
+    $requests[$cr]['Username'] = $row['userName'];
+    $cr++;
+  }
+  
+  echo json_encode(['data'=>$requests]); 
+  // http_response_code(400);
+}
+else
+{
+  http_response_code(404);  
+}
