d50b748f2eb1c991ddbf284a4a7a64afe7bfe04f
Aayog <1aayogkoirala@gmail.com>
2020-11-20 02:10:32 -0500

    Adding approval reviewing

--- php/deleterequest.php
+++ php/deleterequest.php
@@ -3,11 +3,15 @@ header("Access-Control-Allow-Origin: *");
 include_once("config.php");
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-
 if(isset($postdata) && !empty($postdata))
 {
-  $id = mysqli_real_escape_string($conn, trim($request->id));
+//   $id = mysqli_real_escape_string($conn, trim($request->id));
   $sql = "DELETE FROM UserRequests WHERE reqId=".$id;
+  echo $sql;
   $conn->query($sql);
+}
+else
+{
+    http_response_code(400);
 }
 ?>
