c31a420f25a12c8e8c440c4371c5402cd1757a33
Aayog <1aayogkoirala@gmail.com>
2020-11-20 02:40:56 -0500

    Finally fixed the API

--- php/approverequest.php
+++ php/approverequest.php
@@ -3,11 +3,21 @@ header("Access-Control-Allow-Origin: *");
 include_once("config.php");
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-
 if(isset($postdata) && !empty($postdata))
-{
-  $id = mysqli_real_escape_string($conn, trim($request->id));
-  $sql = "UPDATE SET approved=TRUE FROM UserRequests WHERE reqId=".$id;
+{   
+  //$id = mysqli_real_escape_string($conn, trim($request->id));
+
+  $sql = "UPDATE SET approved=TRUE FROM UserRequests WHERE reqId='".$request."'";
+  
+  echo $sql;
   $conn->query($sql);
 }
+else
+{
+    http_response_code(400);
+}
 ?>
+
+
+
+
--- php/deleterequest.php
+++ php/deleterequest.php
@@ -4,9 +4,9 @@ include_once("config.php");
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 if(isset($postdata) && !empty($postdata))
-{
-//   $id = mysqli_real_escape_string($conn, trim($request->id));
-  $sql = "DELETE FROM UserRequests WHERE reqId=".$id;
+{   
+  //$id = mysqli_real_escape_string($conn, trim($request->id));
+  $sql = "DELETE FROM UserRequests WHERE reqId='".$request."'";
   echo $sql;
   $conn->query($sql);
 }
